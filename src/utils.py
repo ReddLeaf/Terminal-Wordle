59f04878da4556109478b3befc825381d73306f7
ReddLeaf <glchalili12@gmail.com>
2026-03-01 03:14:58 +0800

    misc: minor code re-organization

--- src/utils.py
+++ src/utils.py
@@ -14,8 +14,8 @@ def _load_words(filename: str) -> set[str]:
     with open(filename, "r") as f:
         return {line.strip().upper() for line in f}
 
-_answers = _load_words("src\\answers_list.txt.")
-_allowed_guesses = _answers | _load_words("src\\other_allowed_guesses.txt")
+_answers = _load_words("src\\words lists\\answers_list.txt")
+_allowed_guesses = _answers | _load_words("src\\words lists\\other_allowed_guesses.txt")
     
 def is_valid_word(word: str) -> bool:
     return word in _allowed_guesses
